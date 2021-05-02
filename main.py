959125ebc422d2de119f773a3ab541b98f7949dd
Alireza Mazochi <armazochi@gmail.com>
2021-05-02 16:22:43 +0430

    Read input excel file

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+from openpyxl import load_workbook
+
+wb = load_workbook("./input_data/IR_Spring2021_ph12_7k.xlsx")
+sheet = wb.active
+
+content_list = []
+url_list = []
+
+for i in range(2, 7002):
+    data_id = int(sheet.cell(i, 1).value)
+    data_content = sheet.cell(i, 2).value
+    data_url = sheet.cell(i, 3).value
+    content_list.append(data_content)
+    url_list.append(data_url)
+
+print(content_list[2])
+
